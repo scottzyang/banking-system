1612133a37c5b6e0368a771006dd4a6acd439cd9
scottzyang <scottzyang@gmail.com>
2022-10-30 18:04:51 -0500

    Set up deposit method

--- BankAccount.py
+++ BankAccount.py
@@ -4,3 +4,11 @@ class BankAccount:
         self.account_number = account_number
         self.balance = balance
 
+    def deposit(self, amount):
+        self.balance += amount
+        print(f"Deposited Amount: {amount}.\nNew Balance: {self.balance}.")
+
+
+
+# scott_account = BankAccount("Scott Yang", 123456, 200)
+# scott_account.deposit(150)
