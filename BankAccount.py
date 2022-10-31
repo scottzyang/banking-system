86d58315b21fccafad7991da231ea3c48b187775
scottzyang <scottzyang@gmail.com>
2022-10-30 19:14:51 -0500

    Add second instantiation

--- BankAccount.py
+++ BankAccount.py
@@ -39,32 +39,29 @@ class BankAccount:
 # create/instantiate 3 bank account examples
 # ---------------------------------------------------------------
 alisha_account = BankAccount("Alisha Hwee", 400)
-# should output "400"
+# output "400"
 alisha_account.get_balance()
-# should output 150
+# output 150
 alisha_account.withdraw(250)
-# should output 175
+# output 175
 alisha_account.deposit(25)
 # add interests
 alisha_account.add_interests()
-# new balance should be 176.45 & account no.
+# outputs new balance 176.45 & account no.
 alisha_account.print_statement()
 
+# ---------------------------------------------------------------
+scott_account = BankAccount("Scott Yang", 5000)
+# outputs 5000
+scott_account.get_balance()
+# rounds to nearest cent and outputs 3999.65
+scott_account.withdraw(1000.3543)
+# rounds to nearest cent and outputs 4200.19
+scott_account.deposit(200.543)
+# add interest and rounds to nearest cent
+scott_account.add_interests()
+# outputs new balance 4235.05 & account no.
+scott_account.print_statement()
 
+# ---------------------------------------------------------------
 
-
-# Tests
-# # Instantiate (create) an account
-# scott_account = BankAccount("Scott Yang", 200)
-# # deposit
-# scott_account.deposit(150)
-# # withdraw and display new balance
-# scott_account.withdraw(100)
-# # withdraw and display insufficient funds
-# scott_account.withdraw(10)
-# # get balance
-# scott_account.get_balance()
-# # add_interests test
-# scott_account.add_interests()
-# # print statement
-# scott_account.print_statement()
